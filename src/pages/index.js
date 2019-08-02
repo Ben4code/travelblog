9865950a4571476edf74aae8f4f2d2ebe2a979e3
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-08-02 16:07:23 +0100

    Added Featured component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import Nav from "../components/nav/index"
 import SEO from "../components/seo"
+import Featured from '../components/featured/index'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Nav/>
+    <Featured/>
   </Layout>
 )
 
